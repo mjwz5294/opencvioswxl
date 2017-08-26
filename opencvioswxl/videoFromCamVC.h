5c4be0df41a95df241cfe2a7f5dc3aa4e41d4ccf
wxl <393578079@qq.com>
2017-08-26 13:57:00 +0800

    添加opencv的video功能，但编译不通过

---
+++ opencvioswxl/videoFromCamVC.h
@@ -0,0 +1,13 @@
+//
+//  videoFromCamVC.h
+//  opencvioswxl
+//
+//  Created by wxl on 2017/8/26.
+//  Copyright © 2017年 mjwz5294. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface videoFromCamVC : UIViewController
+
+@end
