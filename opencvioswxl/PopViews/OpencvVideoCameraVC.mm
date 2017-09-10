c1e039f22c4aefc6a1f8ccfcee618c772974081d
mjwz5294 <393578079@qq.com>
2017-09-10 16:01:31 +0800

    添加“复古效果”

--- opencvioswxl/PopViews/OpencvVideoCameraVC.mm
+++ opencvioswxl/PopViews/OpencvVideoCameraVC.mm
@@ -12,17 +12,24 @@
 #import <opencv2/imgcodecs/ios.h>
 #import <opencv2/videoio/cap_ios.h>
 
+#import "RetroFilter.hpp"
+#import <mach/mach_time.h>
+
 @interface OpencvVideoCameraVC ()<CvVideoCameraDelegate>{
     void (^callback_)(UIImage*);
+    
 }
 
 @property (nonatomic, weak) IBOutlet UIImageView* imageView;
 @property (nonatomic, weak) IBOutlet UIButton* showToolBtn;
-@property (nonatomic, assign) BOOL isFocusLocked, isExposureLocked, isBalanceLocked;
+@property (nonatomic, assign) BOOL isFocusLocked, isExposureLocked, isBalanceLocked, isUsingRetroFilter;
 
 
 @property (nonatomic, strong) CvVideoCamera* videoCamera;
 @property (nonatomic, strong) UIImage* img;
+@property (nonatomic, assign) RetroFilter::Parameters params;
+@property (nonatomic, assign) cv::Ptr<RetroFilter> filter;
+@property (nonatomic, assign) uint64_t prevTime;
 
 @end
 
@@ -67,8 +74,25 @@
     _isFocusLocked = NO;
     _isExposureLocked = NO;
     _isBalanceLocked = NO;
+    _isUsingRetroFilter = NO;
+    
+    // Load textures
+    UIImage* resImage = [UIImage imageNamed:@"scratches.png"];
+    UIImageToMat(resImage, _params.scratches);
+    
+    resImage = [UIImage imageNamed:@"fuzzyBorder.png"];
+    UIImageToMat(resImage, _params.fuzzyBorder);
+    
+    _prevTime = mach_absolute_time();
     
     [_videoCamera start];
+    
+    _params.frameSize = cv::Size(_videoCamera.imageWidth,
+                                _videoCamera.imageHeight);
+    
+    if (!_filter)
+        _filter = new RetroFilter(_params);
+    
 }
 
 - (IBAction)showTools:(id)sender {
@@ -102,6 +126,9 @@
     void (^rotationBlock)(CGFloat,NSString*,BOOL) = ^(CGFloat progress,NSString* titleStr,BOOL isSwitchOn){
         weakSelf.videoCamera.rotateVideo = isSwitchOn;
     };
+    void (^usingRetroFilterBlock)(CGFloat,NSString*,BOOL) = ^(CGFloat progress,NSString* titleStr,BOOL isSwitchOn){
+        weakSelf.isUsingRetroFilter = isSwitchOn;
+    };
     void (^stopBlock)() = ^(){
         [weakSelf.videoCamera stop];
         if(weakSelf.img){
@@ -115,14 +142,63 @@
                                              @{@"callback":lockExposureBlock,@"title":@"曝光开关",@"isSwitch":@(_isExposureLocked)},
                                              @{@"callback":lockBalanceBlock,@"title":@"平衡开关",@"isSwitch":@(_isBalanceLocked)},
                                              @{@"callback":rotationBlock,@"title":@"旋转开关",@"isSwitch":@(_videoCamera.rotateVideo)},
+                                             @{@"callback":usingRetroFilterBlock,@"title":@"复古效果",@"isSwitch":@(_isUsingRetroFilter)},
                                              @{@"callback":stopBlock,@"title":@"关闭"}
                                              ] OtherParms:@{@"parentView":self.view}];
+}
+
+// Macros for time measurements
+#if 1
+#define TS(name) int64 t_##name = cv::getTickCount()
+#define TE(name) printf("TIMER_" #name ": %.2fms\n", \
+1000.*((cv::getTickCount() - t_##name) / cv::getTickFrequency()))
+#else
+#define TS(name)
+#define TE(name)
+#endif
+
+//TODO: may be remove this code
+static double machTimeToSecs(uint64_t time)
+{
+    mach_timebase_info_data_t timebase;
+    mach_timebase_info(&timebase);
+    return (double)time * (double)timebase.numer /
+    (double)timebase.denom / 1e9;
 }
 
 #pragma mark- CvVideoCameraDelegate
 - (void)processImage:(cv::Mat&)image
 {
     // Do some OpenCV processing with the image
+    
+    
+    if (_isUsingRetroFilter) {
+        cv::Mat inputFrame = image;
+        BOOL isNeedRotation = image.size() != _params.frameSize;
+        if (isNeedRotation)
+            inputFrame = image.t();
+        
+        // Apply filter
+        cv::Mat finalFrame;
+        TS(ApplyingFilter);
+        _filter->applyToVideo(inputFrame, finalFrame);
+        TE(ApplyingFilter);
+        
+        if (isNeedRotation)
+            finalFrame = finalFrame.t();
+        
+        // Add fps label to the frame
+        uint64_t currTime = mach_absolute_time();
+        double timeInSeconds = machTimeToSecs(currTime - _prevTime);
+        _prevTime = currTime;
+        double fps = 1.0 / timeInSeconds;
+        NSString* fpsString = [NSString stringWithFormat:@"FPS = %3.2f", fps];
+        cv::putText(finalFrame,[fpsString UTF8String],cv::Point(30, 30),cv::FONT_HERSHEY_COMPLEX_SMALL,0.8,cv::Scalar::all(255));
+        
+        finalFrame.copyTo(image);
+    }
+    
+    
     _img = MatToUIImage(image);
 }
 
